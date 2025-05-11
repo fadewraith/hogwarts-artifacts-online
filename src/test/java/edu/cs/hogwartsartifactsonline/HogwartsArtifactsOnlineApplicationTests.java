83dbd4500bfd0fb87468727da87ab09056179cba
fadewraith <82732378+fadewraith@users.noreply.github.com>
2025-05-11 12:14:23 +0530

    Update HogwartsArtifactsOnlineApplicationTests.java

--- src/test/java/edu/cs/hogwartsartifactsonline/HogwartsArtifactsOnlineApplicationTests.java
+++ src/test/java/edu/cs/hogwartsartifactsonline/HogwartsArtifactsOnlineApplicationTests.java
@@ -8,6 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles(value = "dev")
 class HogwartsArtifactsOnlineApplicationTests {
 
+	// https://www.youtube.com/playlist?list=PLqq9AhcMm2oPdXXFT3fzjaKLsVymvMXaY
+
 	@Test
 	void contextLoads() {
 	}
